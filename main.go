8b0d7f271a1b8fc22f5ba05b4674843b29a96749
Omoba <ogabiprince@gmail.com>
2024-02-29 17:57:22 +0100

    Added html files

--- main.go
+++ main.go
@@ -9,15 +9,36 @@ import (
 func main() {
 
 	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
-        filePath := "." + r.URL.Path
-        http.ServeFile(w, r, filePath)
-    })
-
+		filePath := "." + r.URL.Path
+		http.ServeFile(w, r, filePath)
+	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// tmpl := template.Must(template.ParseFiles("dashboard.html"))
 		// tmpl.Execute(w, r)
 		tmpl, err := template.ParseFiles("dashboard.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		tmpl.Execute(w, nil)
+		// http.ServeFile(w, r, "dashboard.html")
+	})
+
+	http.HandleFunc("/bestpractices", func(w http.ResponseWriter, r *http.Request) {
+		// tmpl := template.Must(template.ParseFiles("dashboard.html"))
+		// tmpl.Execute(w, r)
+		tmpl, err := template.ParseFiles("best_practices.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		tmpl.Execute(w, nil)
+		// http.ServeFile(w, r, "dashboard.html")
+	})
+
+	http.HandleFunc("/bestpractices/vm", func(w http.ResponseWriter, r *http.Request) {
+		// tmpl := template.Must(template.ParseFiles("dashboard.html"))
+		// tmpl.Execute(w, r)
+		tmpl, err := template.ParseFiles("./cloud_blog/article_VMs.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
